26cf1de0f584e27616720cf3e83658f7fd1f395e
CrnZ1675 <crn.alexei@gmail.com>
2024-11-09 17:10:59 +0200

    Reworked coefficient and number inquire system

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,22 +1,37 @@
-﻿string expression = Console.ReadLine()!;
-decimal coefficient = 0;
+﻿decimal coefficient = 0;
 decimal number = 0;
+
+Console.Write("Write your expression (ax + bx +... = c: ");
+string expression = Console.ReadLine()!;
 
 for (int i = 0; i < expression.Length; i++)
 {
     if (expression[i] == 'x')
     {
-        coefficient = decimal.Parse(expression[i - 1].ToString());
-        Console.WriteLine(coefficient);
+        decimal temp = decimal.Parse(expression[i - 1].ToString());
+        coefficient += temp;
+        Console.WriteLine(temp);
     }
-    else if (expression[i + 1] == ' ' && (expression[i] == '1' || expression[i] == '2' ||
-                                          expression[i] == '3' || expression[i] == '4' || expression[i] == '5' ||
-                                          expression[i] == '6' || expression[i] == '7' || expression[i] == '8' ||
-                                          expression[i] == '9'))
+
+    bool isNumber = expression[i] == '1' || expression[i] == '2' || expression[i] == '3' || 
+                    expression[i] == '4' || expression[i] == '5' || expression[i] == '6' || 
+                    expression[i] == '7' || expression[i] == '8' || expression[i] == '9';
+    if (i + 1 >= expression.Length)
     {
-        number = decimal.Parse(expression[i].ToString());
-        Console.WriteLine(number);
+        if (isNumber)
+        {
+            number = decimal.Parse(expression[i].ToString());
+            Console.WriteLine(number);
+        }
+    }
+    else
+    {
+        if (expression[i + 1] == ' ' && isNumber)
+        {
+            number = decimal.Parse(expression[i].ToString());
+            Console.WriteLine(number);
+        }
     }
 }
 
-Console.WriteLine($"Your result is: {number / coefficient}");
+Console.WriteLine($"Your result: {number / coefficient}");
